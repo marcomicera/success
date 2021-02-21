728e05664d7f037bb7e806fb3873f766e741ab30
Marco Micera <marco.micera@endocode.com>
2021-02-21 22:35:52 +0100

    Added single experiment simulation code

---
+++ sim.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"sort"
+)
+
+type Applicant struct {
+	skills int
+	luck   int
+	total  float32
+}
+
+// Applicant constructor
+func NewApplicant() Applicant {
+	applicant := Applicant{
+		skills: rand.Intn(100),
+		luck:   rand.Intn(100),
+	}
+	applicant.total = float32(applicant.skills)*0.95 + float32(applicant.luck)*0.05
+	return applicant
+}
+
+func main() {
+
+	// Params
+	seed := flag.Int64("seed", 20, "Simulation seed")
+	applicantsNum := flag.Int("applicants", 10000, "Number of applicants")
+	spaces := flag.Int("spaces", 11, "Number of applicants that will be selected")
+	flag.Parse()
+
+	// There cannot be more spaces than applicants
+	if *spaces >= *applicantsNum {
+		log.Fatalf("%d spaces for %d applicants. Terminating...", *spaces, *applicantsNum)
+	}
+	if *spaces <= 1 {
+		log.Fatalf("Invalid spaces number: %d. Terminating...", *spaces)
+	}
+
+	rand.Seed(*seed)
+
+	var applicants []Applicant
+
+	// Score generation
+	for i := 0; i < *applicantsNum; i++ {
+		applicants = append(applicants, NewApplicant())
+	}
+
+	// Selection process
+	sort.Slice(applicants, func(i, j int) bool {
+		return applicants[i].total > applicants[j].total
+	})
+
+	// Computing results
+	var avgLuck float32 = 0.0
+	for _, topApplicant := range applicants[:(*spaces)] {
+		avgLuck = avgLuck + float32(topApplicant.luck)
+	}
+	avgLuck = avgLuck / float32(*spaces)
+	fmt.Printf("Average luck score of the top %d applicants: %f\n", *spaces, avgLuck)
+}
